c90470e7afd5c7f14e9abf0bb30ee355a0c6726e
Caspar Ekroth <caspar.ekroth@gmail.com>
2025-04-19 10:46:21 +0200

    now you can search for stations and get the 4 nex departures

--- index.js
+++ index.js
@@ -1,58 +1,5 @@
 const myH1 = document.getElementById("myH1");
-
-async function fetchDepartures(siteId) {
-    try {
-        const response = await fetch(`https://transport.integration.sl.se/v1/sites/${siteId}/departures`);
-        const data = await response.json();
-        console.log(data.departures);
-        return data;
-    } catch (err) {
-        console.error("Error fetching departures:", err);
-    }
-}
-
-async function updatingDisplay(departure,index){
-    console.log(departure,index);
-    const line = departure.line.id;
-    const destination = departure.destination;
-    const time = departure.display;
-    const display = document.getElementById(`output${index}`);
-    display.textContent =`${line} ${destination} ${time}`;
-}
-
-async function displayingData(SITE_ID){
-    const metaRefresh = document.querySelector('meta[http-equiv="refresh"]');
-    if(metaRefresh){
-        getTime();
-        const buses = await fetchDepartures(SITE_ID);
-        let n = 0;
-        for(let i = 0; i<buses.departures.length;i++){
-            if(buses.departures[i].destination == "Mörby station" ||
-                buses.departures[i].destination == "Brommaplan" ||
-                buses.departures[i].destination == "Brommaplan (bussbyte)"){
-                n++;
-                updatingDisplay(buses.departures[i],n);
-            }
-        }
-    }
-}
-
-function getTime(){
-    const now = new Date();
-    let hours = now.getHours();
-    hours = hours.toString().padStart(2,0);
-    const minutes = now.getMinutes().toString().padStart(2,0);
-    const clock = document.getElementById("curentTime");
-    clock.textContent = `${hours}:${minutes}`;
-}
-
-
-//const SITE_ID = "3065";//träkvista
-//displayingData(SITE_ID)
-myH1.textContent = "Träkvista";
-
 const csvUrl = 'https://raw.githubusercontent.com/thuma/StorstockholmsLokaltrafikAPI/master/sl.csv';
-const newUrl = 'https://raw.githubusercontent.com/thuma/StorstockholmsLokaltrafikAPI/master/sl.csv'
 
 async function createJsonFromCSV(url){
     try {
@@ -80,6 +27,84 @@ async function createJsonFromCSV(url){
     }
 }
 
+async function fetchDepartures(siteId) {
+    try {
+        const response = await fetch(`https://transport.integration.sl.se/v1/sites/${siteId}/departures`);
+        const data = await response.json();
+        console.log(data.departures);
+        return data;
+    } catch (err) {
+        console.error("Error fetching departures:", err);
+    }
+}
+
+async function updatingDisplay(departure,index){
+    console.log(departure,index);
+
+    const display = document.getElementById(`output${index}`);
+    if (!display) {
+        console.warn(`No element with id="output${index}"`);
+        return;
+    }
+    const line = departure.line.id;
+    const destination = departure.destination;
+    const time = departure.display;
+    display.textContent =`${line} ${destination} ${time}`;
+}
+
+async function displayingData(SITE_ID){
+    //const metaRefresh = document.querySelector('meta[http-equiv="refresh"]');
+    if(true){
+        getTime();
+        fetchDepartures(SITE_ID)
+        .then(data =>{
+            buses = data;
+            for( let i = 0;i < 4; i++){
+                updatingDisplay(buses.departures[i],i);
+            }
+        });
+    }
+}
+
+function getTime(){
+    const now = new Date();
+    let hours = now.getHours();
+    hours = hours.toString().padStart(2,0);
+    const minutes = now.getMinutes().toString().padStart(2,0);
+    const clock = document.getElementById("curentTime");
+    clock.textContent = `${hours}:${minutes}`;
+}
+
+
+//const SITE_ID = "3065";//träkvista
+//displayingData(SITE_ID)
+let dataForID = [];
 createJsonFromCSV(csvUrl)
+    .then(data => {
+    dataForID = data;
+    console.log("Data loaded:", dataForID);
+    })
+    .catch(error => {
+        console.error("Error loading CSV data:", error);
+    });
+
+function refresh(){
+    const input = document.getElementById('searchInput').value;
+    myH1.textContent = input;
+    const index = dataForID.findIndex(item => item.name.toLowerCase() === input.toLowerCase());
+    if(index !== -1){
+        console.log(`found at ${index} and the station id is ${dataForID[index].id}`);
+        //fetchDepartures(dataForID[index].id)
+        //    .then(info => {
+        //        let selectedStop = info;
+        //        console.log(selectedStop.name)
+        //    });
+        displayingData(dataForID[index].id);
+    }else{
+        console.log("not found");
+    }
+}
+
+
 
 
